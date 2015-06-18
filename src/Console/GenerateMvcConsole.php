6bf6bfb1abafb4ffdd2a7c5b4c70d09af77525f4
Kemal Kanok <kemalkanok@gmail.com>
2015-06-18 12:46:11 +0300

    general refactoring

--- src/Console/GenerateMvcConsole.php
+++ src/Console/GenerateMvcConsole.php
@@ -3,6 +3,7 @@
 
 use Illuminate\Console\Command;
 use Kanok\Generators\Libs\Config;
+use Kanok\Generators\Libs\FileHandler;
 
 class GenerateMvcConsole extends Command {
 
@@ -39,13 +40,60 @@ class GenerateMvcConsole extends Command {
 	 */
 	public function fire()
 	{
-
-        dd ((new Config())->get('package'));
-        $this->info('completed: Package Generated Successfully');
+        $data = $this->getInput();
+        dd($data);
     }
 
+    private function getInput()
+    {
+        $tableName = $this->ask('Please Enter the table name');
 
+        $state = $this->askWithCompletion('What would you like to do?[Create/Alter]',['Create','Alter']);
 
+        $fields = $this->getFields();
+
+        $type = $this->askWithCompletion('What would you like to do?['.join('/',$this->getTypes()).']',$this->getTypes());
+
+        return (object)compact('tableName', 'fields','state','type');
+    }
+
+    /**
+     * @return array
+     * @internal param $types
+     */
+    private function getTypes()
+    {
+        $data = (new Config())->get('packages');
+        foreach ($data as $key => $cols) {
+            $types[] = $key;
+        }
+        return  $types;
+    }
+
+    /**
+     * @return array
+     */
+    private function getFields()
+    {
+        $this->info('Please enter the specific fields for creating or altering table, To stop enter "stop"');
+        $i = 1;
+        $fields = [];
+        while (true) {
+            $field = $this->ask($i . '.Field Specs:');
+            if (trim($field) == "stop") {
+                break;
+            }
+
+            $field = explode(':', $field);
+            array_slice($field, 2);
+            $key = $field[0];
+            unset($field[0]);
+            $fields[$key] = $field;
+
+            $i++;
+        }
+        return $fields;
+    }
 
 
 }

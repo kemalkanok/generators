30b5c8a7615fc4212d0c6f463cbd1894beb6f00c
Kemal Kanok <kemalkanok@gmail.com>
2015-06-18 11:57:32 +0300

    new file reading added

--- src/Libs/FileHandler.php
+++ src/Libs/FileHandler.php
@@ -5,9 +5,27 @@ namespace Kanok\Generators\Libs;
 
 class FileHandler {
 
-    function readFromFile($file)
+    /**
+     * reads from a file
+     * @param $file
+     * @param null $basePath
+     * @return string
+     */
+    function readFileFromBundle($file,$basePath = null)
     {
-        return file_get_contents($file);
+        if(!$basePath)
+        {
+            $basePath = __DIR__ .'/'. (new Config())->get('stubPath');
+        }
+        return file_get_contents($basePath.$file);
+    }
+    function readFileFromApp($file,$basePath = null)
+    {
+        if(!$basePath)
+        {
+            $basePath = app_path() .'/../'. (new Config())->get('basePath');
+        }
+        return file_get_contents($basePath.$file);
     }
 
 }

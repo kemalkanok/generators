2c6176cfa4554777d5294e607c69834ad33ec838
Kemal Kanok <kemalkanok@gmail.com>
2015-09-16 16:52:44 +0300

    paypal multi cart added

--- src/Libs/Payments/PaypalJob.php
+++ src/Libs/Payments/PaypalJob.php
@@ -19,104 +19,100 @@ use PayPal\Rest\ApiContext;
 
 class PaypalJob {
 
-    /**
-     * @var Payer
-     */
-    private $payer;
-    /**
-     * @var ItemList
-     */
-    private $itemList;
-    /**
-     * @var Amount
-     */
-    private $amount;
+	/**
+	 * @var Payer
+	 */
+	private $payer;
+	/**
+	 * @var ItemList
+	 */
+	private $itemList;
+	/**
+	 * @var Amount
+	 */
+	private $amount;
 
-    public function addItem($name , $price , $quantity)
-    {
-        $item = new Item();
-        $item->setName($name)
-            ->setCurrency(env('PAYPAL_CURRENCY'))
-            ->setQuantity($quantity)
-            ->setPrice($price);
-        $this->itemList->addItem($item);
-    }
+	public function addItem($name, $price, $quantity) {
+		$item = new Item();
+		$item->setName($name)
+		     ->setCurrency(env('PAYPAL_CURRENCY'))
+		     ->setQuantity($quantity)
+		     ->setPrice($price);
+		$this->itemList->addItem($item);
+	}
 
-    public function __construct()
-    {
-        $this->handle();
-    }
+	public function __construct() {
+		$this->handle();
+	}
 
-    public function calcAmount()
-    {
-        $this->amount = new Amount();
-        $this->amount->setCurrency(env('PAYPAL_CURRENCY'));
+	public function calcAmount() {
+		$this->amount = new Amount();
+		$this->amount->setCurrency(env('PAYPAL_CURRENCY'));
 
-        $total = 0;
-        foreach($this->itemList->items as $item)
-        {
-            $total += $item->price;
-        }
-        $this->amount->setTotal($total);
-    }
+		$total = 0;
+		foreach ($this->itemList->items as $item) {
+			$total += $item->price*$item->quantity;
+		}
+		$this->amount->setTotal($total);
+	}
 
-    public function handle()
-    {
-        $this->payer = new Payer();
-        $this->payer->setPaymentMethod("paypal");
-        $this->itemList = new ItemList();
+	public function handle() {
+		$this->payer = new Payer();
+		$this->payer->setPaymentMethod("paypal");
+		$this->itemList = new ItemList();
 
-    }
+	}
 
-    /**
-     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
-     */
-    public function payment()
-    {
+	/**
+	 * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+	 */
+	public function payment() {
 
-        $this->calcAmount();
-        $transaction = new Transaction();
-        $transaction->setAmount($this->amount)
-            ->setItemList($this->itemList)
-            ->setDescription("Payment description")
-            ->setInvoiceNumber(uniqid());
-        $baseUrl = env('APP_URL');
-        $redirectUrls = new RedirectUrls();
-        $redirectUrls->setReturnUrl("$baseUrl/test/success")
-            ->setCancelUrl("$baseUrl/test/fail");
-        $payment = new Payment();
-        $payment->setIntent("sale")
-            ->setPayer($this->payer)
-            ->setRedirectUrls($redirectUrls)
-            ->setTransactions(array($transaction));
-        //$request = clone $payment;
-        $apiContext = new ApiContext(
-            new OAuthTokenCredential(
-                env('PAYPAL_CLIENT_ID'),
-                env('PAYPAL_SECRET')
-            )
-        );
+		$this->calcAmount();
+		$transaction = new Transaction();
+		$transaction->setAmount($this->amount)
+			->setItemList($this->itemList)
+			->setDescription("Payment description")
+			->setInvoiceNumber(uniqid());
+		$baseUrl      = env('APP_URL');
+		$redirectUrls = new RedirectUrls();
+		$redirectUrls->setReturnUrl("$baseUrl/test/success")
+		             ->setCancelUrl("$baseUrl/test/fail");
+		$payment = new Payment();
+		$payment->setIntent("sale")
+		        ->setPayer($this->payer)
+			->setRedirectUrls($redirectUrls)
+			->setTransactions(array($transaction));
+		//$request = clone $payment;
+		$apiContext = new ApiContext(
+			new OAuthTokenCredential(
+				env('PAYPAL_CLIENT_ID'),
+				env('PAYPAL_SECRET')
+			)
+		);
 
-        $apiContext->setConfig(
-            array(
-                'mode' => 'sandbox',
-                'log.LogEnabled' => true,
-                'log.FileName' => '../PayPal.log',
-                'log.LogLevel' => 'DEBUG', // PLEASE USE `FINE` LEVEL FOR LOGGING IN LIVE ENVIRONMENTS
-                'validation.level' => 'log',
-                'cache.enabled' => true,
-                // 'http.CURLOPT_CONNECTTIMEOUT' => 30
-                // 'http.headers.PayPal-Partner-Attribution-Id' => '123123123'
-            )
-        );
+		$apiContext->setConfig(
+			array(
+				'mode'             => 'sandbox',
+				'log.LogEnabled'   => true,
+				'log.FileName'     => '../PayPal.log',
+				'log.LogLevel'     => 'DEBUG', // PLEASE USE `FINE` LEVEL FOR LOGGING IN LIVE ENVIRONMENTS
+				'validation.level' => 'log',
+				'cache.enabled'    => true,
+				// 'http.CURLOPT_CONNECTTIMEOUT' => 30
+				// 'http.headers.PayPal-Partner-Attribution-Id' => '123123123'
+			)
+		);
 
-        try {
-            $payment->create($apiContext);
-        } catch (Exception $ex) {
-            dd($ex->getMessage());
-            exit(1);
-        }
-        $approvalUrl = $payment->getApprovalLink();
-        return redirect($approvalUrl);
-    }
+		try {
+			$payment->create($apiContext);
+		} catch (Exception $ex) {
+			dd($ex->getMessage());
+			exit(1);
+		}
+		$approvalUrl = $payment->getApprovalLink();
+		$token       = substr($approvalUrl, strripos($approvalUrl, "token")+6);
+		session()->put('_payment.payment_token', $token);
+		return redirect($approvalUrl);
+	}
 }

c750178796a4f6b59e300bd8cf259583d3688124
Kemal Kanok <kemalkanok@gmail.com>
2015-09-21 12:54:15 +0300

    resize function added

--- src/Framework/Helpers/FileUpload.php
+++ src/Framework/Helpers/FileUpload.php
@@ -10,10 +10,12 @@ namespace Kanok\Generators\Framework\Helpers;
 
 
 
+
+
 class FileUpload {
 
     /**
-     * uploads a file with unique user name and returns the new file name
+     * uploads a file with unique file name and returns the new file name
      * @param $request
      * @param $field
      * @param $path
@@ -22,9 +24,18 @@ class FileUpload {
     public static function uniqueUpload($request,$field,$path)
     {
         if ($pic = $request->file($field)) {
+
+
             $extension = $pic->getClientOriginalExtension();
-            $filename = uniqid() . "." . $extension;
+            $uniqueName = uniqid();
+            $filename =  $uniqueName . "." . $extension;
             $pic->move($path, $filename);
+            //create resizes pics for theme
+            foreach(config('theme.sizes') as $suffix => $size)
+            {
+                \Image::make($path.$filename)->resize($size[0], $size[1])->save($path. $uniqueName.'.'.$suffix.'.'.$extension);
+            }
+
             return '/'.$path . $filename;
         }
     }
